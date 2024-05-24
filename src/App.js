ffe2959d8dae9d569217f5be24105dfa1dedded6
Steve <steve.gitprofile@outlook.com>
2024-05-24 12:40:51 +0200

    added reusable buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  // want step to be dynamic
+  //  need piece of state
+  // const step = 2;
+
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step === 1) {
+      return;
+    }
+    // if set state based on current state use call back to retrieve current state
+    setStep((s) => s - 1);
+  }
+
+  function handleNext() {
+    if (step === 3) {
+      return;
+    }
+    // callback to get current state
+    setStep((s) => s + 1);
+  }
+
+  //  cant just return { } with a conditional render for isOpen
+  // needs to be inside a component - div or fragement to start JSX (NB to remember)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* dont always have to write function first can use annonymouse */}
+      <button className="close" onClick={() => setIsOpen((isO) => !isO)}>
+        {isOpen ? "×" : "⊕"}
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <p className="message">
+            {step > 0 && `Step ${step}: ${messages[step - 1]}`}
+          </p>
+
+          <div className="buttons">
+            <Button
+              bcolor="#7950f2"
+              color="#fff"
+              text="Previous"
+              onClick={handlePrevious}
+            />
+            <Button
+              bcolor="#7950f2"
+              color="#fff"
+              text="Next"
+              onClick={handleNext}
+            />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
-export default App;
+function Button({ color, bcolor, onClick, text }) {
+  return (
+    <button style={{ backgroundColor: bcolor, color: color }} onClick={onClick}>
+      {text}
+    </button>
+  );
+}
